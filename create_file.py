5c5def0abc906878d3d3eddcb6ab7a03a5fdd9cb
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2020-01-17 11:00:11 -0300

    Added HDF5 hard link in Python code.

--- create_file.py
+++ create_file.py
@@ -3,9 +3,12 @@ import h5py
 file = h5py.File('from_python.h5', 'w')
 file.attrs["name"] = "test file"
 
+fileDataset = file.create_dataset("complex vector", data=[[0, 1, 2], [0, 10, 20]])
+
 group = file.create_group("group example")
 group.attrs["number"] = 3
 groupDataset = group.create_dataset("simple vector", (1,3), dtype=int)
 groupDataset[:] = [0, 1, 2]
 
-fileDataset = file.create_dataset("complex vector", data=[[0, 1, 2], [0, 10, 20]])
+groupSI = file.create_group("something inside") # SI: something inside
+groupSI["group with things"] = group
