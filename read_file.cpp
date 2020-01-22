89d39710487a89e770dd1bf47d36c53b1f43a3c6
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2020-01-22 08:21:27 -0300

    Made C++ program for reading data from designed file.

---
+++ read_file.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <vector>
+#include <H5Cpp.h>
+
+int main()
+{
+	try
+	{
+		// File
+		std::string filePath = "from_cpp.h5";
+		H5::H5File file(filePath, H5F_ACC_RDONLY);
+		std::cout << "File '" << filePath << "' opened." << std::endl;
+
+		{
+			// File attribute
+			std::string attrName  = "name";
+			H5::Attribute attribute = file.openAttribute(attrName);
+			std::cout << "\tAttribute '" << attribute.getName() << "' opened." << std::endl;
+			// Attribute data
+			std::string attrData;
+			attribute.read(attribute.getStrType(), attrData);
+			std::cout << "\t\tAttribute '" << attribute.getName() << "' data read." << std::endl;
+			std::cout << "\t\tContent: " << attrData << std::endl;
+			attribute.close();
+		}
+
+		{
+			// File dataset
+			std::string datasetName = "complex vector";
+			H5::DataSet dataset = file.openDataSet(datasetName);
+			std::cout << "\tDataset '" << datasetName << "' opened." << std::endl;
+			// Dataset data
+			std::vector<int> datasetData;
+			datasetData.resize(6);
+			dataset.read(datasetData.data(), H5::PredType::NATIVE_INT);
+			std::cout << "\t\tDataSet '" << datasetName << "' data read." << std::endl;
+			std::cout << "\t\tContent: {";
+			for(auto& val: datasetData)
+				std::cout << val << ", ";
+			std::cout << "}" << std::endl;
+			dataset.close();
+		}
+		
+		// Group
+		std::string groupName = "group example";
+		H5::Group group = file.openGroup(groupName);
+		std::cout << "Group '" << groupName << "' opened." << std::endl;
+
+		{
+			// Group attribute
+			std::string attrName  = "number";
+			H5::Attribute attribute = group.openAttribute(attrName);
+			std::cout << "\tAttribute '" << attribute.getName() << "' opened." << std::endl;
+			// Attribute data
+			int attrData;
+			attribute.read(attribute.getIntType(), &attrData);
+			std::cout << "\t\tAttribute '" << attribute.getName() << "' data read." << std::endl;
+			std::cout << "\t\tContent: " << attrData << std::endl;
+			attribute.close();
+		}
+
+		{
+			// Group dataset
+			std::string datasetName = "simple vector";
+			H5::DataSet dataset = group.openDataSet(datasetName);
+			std::cout << "\tDataset '" << datasetName << "' opened." << std::endl;
+			// Dataset data
+			std::vector<int> datasetData;
+			datasetData.resize(3);
+			dataset.read(datasetData.data(), H5::PredType::NATIVE_INT);
+			std::cout << "\t\tDataSet '" << datasetName << "' data read." << std::endl;
+			std::cout << "\t\tContent: {";
+			for(auto& val: datasetData)
+				std::cout << val << ", ";
+			std::cout << "}" << std::endl;
+			dataset.close();
+		}
+
+		group.close();
+		file.close();
+	}
+	catch (H5::FileIException fileException)
+	{
+		std::cout << "File exception!" << std::endl;
+		std::cout << fileException.getDetailMsg() << std::endl;
+	}
+	catch (H5::AttributeIException attributeException)
+	{
+		std::cout << "Attribute exception!" << std::endl;
+		std::cout << attributeException.getDetailMsg() << std::endl;
+	}
+	catch (H5::GroupIException groupException)
+	{
+		std::cout << "Group exception!" << std::endl;
+		std::cout << groupException.getDetailMsg() << std::endl;
+	}
+	catch (H5::DataSetIException datasetException)
+	{
+		std::cout << "DataSet exception!" << std::endl;
+		std::cout << datasetException.getDetailMsg() << std::endl;
+	}
+	return 0;
+}
